73a167504c9d3309c555806b2d85d7ba6455dc50
Mastrojanni <agrassi2135n@gmail.com>
2021-12-15 20:40:27 +0100

    Proper BEM Refactor

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import CardCounter from "./components/CardCounter/CardCounter";
 
 import "./main.scss";
 
+
 function App() {
     return (
         <div className='App'>
 
-            <header className='layout-center'>
+            <header className='layout--center'>
                 <img src={reduxIcon} alt='redux icon' />
                 <h1>basic redux testing: "counter example"</h1>
             </header>
 
             <main>
-                <section className='layout-center'>
-                    <div className='page--layout-content layout-center'>
+                <section className='layout--center'>
+                    <div className='page-layout__content layout--center'>
 
                         {/* test component */}
                         <CardCounter />
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from './core/store/index';
 
+
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
